da13dbca52921e4d309a4434d8cac0b529bbc0e1
LukaDavies <162514942+LukaDavies@users.noreply.github.com>
2024-09-05 10:15:26 +1000

    graah

--- 06.loopLogic/06.loopLogic.ino
+++ 06.loopLogic/06.loopLogic.ino
@@ -35,22 +35,46 @@
     
 */
 
-static unsigned int LEDpin = 3;
+#define LEDpin 3
+#define BTNpin 4
 
 void setup() 
 {
+ pinMode(BTNpin, INPUT);
  pinMode(LEDpin, OUTPUT);
+ Serial.begin(9600);
 }
 
 void loop() 
 {
+  do {
+    digitalWrite(LEDpin, HIGH);
+    Serial.println("looping");
+  }
+ while (digitalRead(BTNpin) < 0);
+ 
+ digitalWrite(LEDpin, LOW);
+ Serial.println("Loop is skipped");
+}
+
+
+
+/*
+ while (digitalRead(BTNpin) > 0) {
+  digitalWrite(LEDpin, HIGH);
+  Serial.println("Looping");
+ }
+ digitalWrite(LEDpin, LOW);
+ Serial.println("Loop is skipped");
+*/
+
+
+
+/*
   for (int i = 0; i < 256; i++)
    {
     unsigned val = i;
     analogWrite(LEDpin,i);
     delay (50);
    }
-  
-}
-
-
+*/
