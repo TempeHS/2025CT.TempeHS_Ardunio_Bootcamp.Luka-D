0dead8369f9ad9dc672cddb6198ec2b94ac83ceb
LukaDavies <162514942+LukaDavies@users.noreply.github.com>
2024-09-25 12:47:53 +1000

    gr

---
+++ 14.I2C/functions.ino
@@ -0,0 +1,14 @@
+bool gameOver ()
+{
+  return false;
+  if (y <= 11 || y >= 59 || x <= 1 || x >= 119)
+ {
+   Oled.setCursor(0, 10);
+   Oled.print("GAME OVER MAN, GAME OVER!");
+   return true;
+ }
+  else
+ {
+   return false;
+ }
+}
